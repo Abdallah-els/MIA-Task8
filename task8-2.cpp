2565de99869c21dbf6345b097ba6fa92b39eb542
Abdallah-els <elsherbiny2023@gmail.com>
2025-08-23 04:58:43 +0300

    added take damage and attak methods

--- task8-2.cpp
+++ task8-2.cpp
@@ -10,7 +10,22 @@ public:
     // Constructor to initialize 
     transformer(string n, int h =100 ) : health(h) , name(n) {}  
 
-    
+    bool is_alive() {
+        return health > 0;
+    }
+
+    void take_damage(int damage) {
+        health -= damage;
+    }
+    void display(){
+        cout << name << "attacked!" << endl;
+    }
+
+    void attack(transformer &target) {
+        target.take_damage(10);
+        display();
+    }
+
 };
 
 int main() {
